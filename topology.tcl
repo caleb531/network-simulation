b24d8f47c43cd58d6890b1970b4476891b7c5f57
Caleb Evans <caleb@calebevans.me>
2016-04-06 16:28:20 -0700

    Add boilerplate topology markup; create nodes

--- topology.tcl
+++ topology.tcl
@@ -1,1 +1,39 @@
-# Add cool stuff here
+# Create simulator object
+set ns [new Simulator]
+
+# Create trace file
+set tracefd [open topology.tr w]
+$ns trace-all $tracefd
+
+# Create required 28 nodes
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+set n4 [$ns node]
+set n5 [$ns node]
+set n6 [$ns node]
+set n7 [$ns node]
+set n8 [$ns node]
+set n9 [$ns node]
+set n10 [$ns node]
+set n11 [$ns node]
+set n12 [$ns node]
+set n13 [$ns node]
+set n14 [$ns node]
+set n15 [$ns node]
+set n16 [$ns node]
+set n17 [$ns node]
+set n18 [$ns node]
+set n19 [$ns node]
+set n20 [$ns node]
+set n21 [$ns node]
+set n22 [$ns node]
+set n23 [$ns node]
+set n24 [$ns node]
+set n25 [$ns node]
+set n26 [$ns node]
+set n27 [$ns node]
+
+# Run simulation
+$ns run
