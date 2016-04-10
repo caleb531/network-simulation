2f7a4c9a0fa6d723f17f7afd7509fba163cb91a9
Kyle Gullicksen <noobtastic101@gmail.com>
2016-04-10 14:12:53 -0700

    Testing

--- topology.tcl
+++ topology.tcl
@@ -1,3 +1,15 @@
+#Constants
+
+set MAX_NODES 28
+
+
+
+
+
+
+
+
+
 # Create simulator object
 set ns [new Simulator]
 
@@ -5,35 +17,24 @@ set ns [new Simulator]
 set tracefd [open topology.tr w]
 $ns trace-all $tracefd
 
+
+
+#INITIALIZATION
+
 # Create required 28 nodes
-set n0 [$ns node]
-set n1 [$ns node]
-set n2 [$ns node]
-set n3 [$ns node]
-set n4 [$ns node]
-set n5 [$ns node]
-set n6 [$ns node]
-set n7 [$ns node]
-set n8 [$ns node]
-set n9 [$ns node]
-set n10 [$ns node]
-set n11 [$ns node]
-set n12 [$ns node]
-set n13 [$ns node]
-set n14 [$ns node]
-set n15 [$ns node]
-set n16 [$ns node]
-set n17 [$ns node]
-set n18 [$ns node]
-set n19 [$ns node]
-set n20 [$ns node]
-set n21 [$ns node]
-set n22 [$ns node]
-set n23 [$ns node]
-set n24 [$ns node]
-set n25 [$ns node]
-set n26 [$ns node]
-set n27 [$ns node]
+
+for {set i 0} {$i < $MAX_NODES} {incr i}
+{
+  nodes($i) [$ns node]
+}
+
+
+#END INITALIZATION
+
+
+
+
+
 
 # Run simulation
 $ns run
