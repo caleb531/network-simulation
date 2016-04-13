6d6b84f3454e50c964bc3d7f006b3ca909e7f438
Kyle Gullicksen <noobtastic101@gmail.com>
2016-04-13 09:28:15 -0700

    best commit message ever,

--- topology.tcl
+++ topology.tcl
@@ -9,6 +9,7 @@ set MAX_NODES 28
 #Globals
 
 global nodes
+global set flowID 0
 
 proc make_blue_node { traffic_src }
 {
@@ -53,6 +54,97 @@ proc make_white_node { traffic_src }
 	$ns attach-agent $white_node $loss_monitor_agent
 
 }
+
+
+
+
+
+
+proc set_events { } 
+{
+	
+}
+
+
+
+
+
+
+proc get_next_flow_id { } 
+{
+	return [incr flowID]
+}
+
+
+#You connect the data source to the data sink
+#FTP over TCP connected to TCPSink (As default, the maximum size of a packet that a "tcp" agent can generate is 1KByte. Don’t change it).
+proc setup_router_ones_agents {  }
+{
+	set ns [Simulator instance]
+	set routerOne = $nodes(1)
+
+	#Setup a TCP connection
+	set tcp [new Agent/TCP]
+	$tcp set class_ 2
+	$ns attach-agent $routerOne $tcp
+	
+
+	#Setup a FTP over TCP connection
+	set ftp [new Application/FTP]
+	$ftp attach-agent $tcp
+	$ftp set type_ FTP
+
+	#return the data source
+	return ftp
+} 
+
+proc setup_router_one_edge_node {edgeNode router_one_ftp}
+{
+	set sink [new Agent/TCPSink]
+	$ns attach-agent $edgeNode $sink
+
+	return sink
+}
+
+
+proc setup_router_one_edge_agents { router_one_ftp lowerIndex upperIndex }
+{
+	for {set i lowerIndex} {i <= upperIndex} {incr i}
+	{
+		set edge_data_sink [setup_router_one_edge_node $nodes(i) router_one_ftp]
+
+		#Connect the router data source to the edge's data sink
+
+		#Connect the edge node's data sink to the router's data source
+		$ns connect $edge_data_sink $router_one_ftp
+
+		#Set a ID for this flow [currently just set linearly]
+		$tcp set fid_ [get_next_flow_id]
+	}
+}
+
+
+#set sink [new Agent/TCPSink]
+#	$ns attach-agent $n3 $sink
+#	$ns connect $tcp $sink
+#	$tcp set fid_ 1
+
+
+proc setup_agents_and_events_in_topology {} 
+{
+
+
+	
+
+}
+
+
+
+
+
+
+
+
 
 #I need a better name!
 proc add_green_duplex_links { lowerIndex upperIndex dest } 
@@ -172,22 +264,6 @@ proc create_nodes { }
 }
 
 
-proc init_topology { }
-{
-	[create_nodes]
-	[interconnect_nodes]
-}
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -201,12 +277,63 @@ proc init_topology { }
 set tracefd [open topology.tr w]
 $ns trace-all $tracefd
 
-
-[init_topology]
-
-#Configure agents
+#Then, setup our events 
+set ns [Simulator instance]
 
 
+[create_nodes]
+[interconnect_nodes]
+
+#setup agents for router 1
+#Router 1
+set router_one_ftp = [setup_router_ones_agents]
+[setup_router_one_edge_agents router_one_ftp 7 13]
+
+
+
+#do this for the other routers...
+
+
+
+
+
+
+
+
+
+#Setup our events
+
+
+
+#At time 1, 9->12, 13->8, 9->14 traffic flows start
+$ns at 1 "$cbr start"
+
+#At time 2, 13->11, 9->15, 13->17 traffic flows start
+$ns at 2 "$cbr start"
+
+#At time 3, 13->19, 9->18, 13->21 traffic flows start
+$ns at 3 "$cbr start"
+
+#At time 4, 13->24, 9->20, 13->25 traffic flows start
+$ns at 4 "$cbr start"
+
+#At time 5, 9->23, 13->26, 9->27 traffic flows start
+$ns at 5 "$cbr start"
+
+#At time 6, 10->18, 16->18 traffic flows start
+$ns at 6 "$cbr start"
+
+#At time 7, link 1-3 goes Down, refer to example4.tcl
+$ns at 7 "$cbr start"
+
+#At time 8, link 1-3 goes Up, refer to example4.tcl
+$ns at 8 "$cbr start"
+
+
+$ns at 9 "$cbr start"
+
+#At time 10, ns stops
+$ns at 10 "$cbr start"
 
 
 
