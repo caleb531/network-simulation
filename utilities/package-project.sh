6982b6c9c6b50c956f91fd489ab9d3d64f091017
Caleb Evans <caleb@calebevans.me>
2016-04-24 15:12:25 -0700

    Update packager script with new topology name

--- utilities/package-project.sh
+++ utilities/package-project.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-tar -cf simulation.tar README.txt topology.tcl Folder9/ Folder10/ Folder13/ Folder16/
+tar -cf \
+	simulation.tar \
+	README.txt \
+	caleb_evans-kyle_gullicksen.tcl \
+	Folder9/ \
+	Folder10/ \
+	Folder13/ \
+	Folder16/
